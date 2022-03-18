fe769ceeb3561baa1cd83428f4dfc9131ef2586f
Dibydo <yaroslav.zhovtyak@gmail.com>
2022-03-18 16:29:01 +0300

    main outline

--- src/main.c
+++ src/main.c
@@ -1,11 +1,21 @@
 #include "../include/indTask/greek.h"
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc > 0) {
+        for (int i = 0; i < argc; i++) {
+            //добавить в массив аргументы
+        }
+    }
+    else {
+        //scanf()
+    }
+    return 0;
+}
+/*
     int output;
     char v[] = "V";
     output = number_feeder(v);
     if (digit('V') == output){
         return (1 == 1);
     }
-    return 0;
-}
+    */
